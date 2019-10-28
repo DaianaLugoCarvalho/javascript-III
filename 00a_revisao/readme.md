03499a22faaeed4babf1e7ee15ba55362df8e4a8
DaianaLugoCarvalho <daianalugo.carvalho@gmail.com>
2019-10-28 16:48:50 -0300

    exercicio Json iniciado

--- 00a_revisao/readme.md
+++ 00a_revisao/readme.md
@@ -1,2 +1,1 @@
 "A Lição Final" LIVRO FAVORITO
-sobre almnkjjsndjcdsjkcnjkdsncjkdsnkjcndsncdskc
--- 01a_ajax_json/index.js
+++ 01a_ajax_json/index.js
@@ -2,3 +2,22 @@
 // [] o que fazer depois que receber a resposta
 // [] fazer a requisição com open() e send()
 // [] tratar response
+const div = document.querySelector('#root');
+const img = document.createElement('img');
+const request = new XMLHttpRequest();
+
+
+request.onreadystatechange = algumafuncao;
+
+function algumafuncao() {
+    console.log('olar');
+
+    const response = request.response;
+    const blob = response.blob();
+    
+    
+    
+}
+const url = './img/cintiafumi.jpeg'
+request.open('GET',url);
+request.send();
